dad14b2f45f6e7854129f4038cc7b896bf2db735
alecyuverymuch <amyu@wisc.edu>
2017-11-16 15:55:31 -0600

    Fix

--- files/P4.java
+++ files/P4.java
@@ -59,7 +59,7 @@ public class P4 {
         if (ErrMsg.fatal == false) {
             ((ASTnode)root.value).nameAnalysis(symTable);
         }
-        System.out.println(symTable.toString());
+        System.out.println(symTable.print());
         if (ErrMsg.fatal == false) {
             ((ASTnode)root.value).unparse(outFile, 0);
         }
